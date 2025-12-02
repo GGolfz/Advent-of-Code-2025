17e6f90e528473086bf1ebe485ec6934ae5129aa
Wisarut Kitticharoenphonngam <52349645+GGolfz@users.noreply.github.com>
2025-12-02 20:34:09 +0700

    Update part 1 to use regex

--- 02/part1.rb
+++ 02/part1.rb
@@ -1,13 +1,8 @@
-result = 0
-readline.split(",").map(&:to_s).each do | range |
-    splitValue = range.split("-")
-    startNum = splitValue[0].to_i
-    endNum = splitValue[1].to_i
-    for value in (startNum..endNum)
-        valueStr = value.to_s
-        if valueStr.length % 2 == 0 && valueStr[0,valueStr.length / 2] == valueStr[valueStr.length / 2,valueStr.length]
-            result += value
-        end
-    end
-end
+values = readline.split(',').flat_map { |range|
+    start_num, end_num = range.split('-').map { |v| v.strip.to_i }
+    (start_num..end_num).to_a
+}
+repeated_pattern = /^(.+)\1$/
+result = values.select { |value| value.to_s.match?(repeated_pattern) }.sum
+
 puts result
